2ab4d54a477f103297b33f55fa1dbd292250edef
jpclipffel <jpclipffel@users.noreply.github.com>
2020-12-07 19:32:31 +0100

    Updated files header

--- dns.sh
+++ dns.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-
-
 function __dns_flush_darwin() {
     sudo killall -HUP mDNSResponder
     sudo killall mDNSResponderHelper
     sudo dscacheutil -flushcache
+    # ---
+    # Force mDNSResponder restart
+    # sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+    # sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+}
+
+
+function __dns_flush_linux() {
+    sudo systemd-resolve --flush-caches
 }
 
 
@@ -12,6 +18,7 @@ function __dns_flush_darwin() {
 function dns_flush() {
     case  $(uname -s) in
         Darwin*) __dns_flush_darwin;;
+        Linux) __dns_flush_linux;;
         *) echo "$(basename $0): Not implemented for $(uname -s) OS" >&2; exit 1;;
     esac
 }
--- nmap.sh
+++ nmap.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-
 function nmap_discover_fast() {
     test -z "${1}" && echo "Usage: nmap_fast <ip/range>" && return 1;
     nmap -sn ${1}
