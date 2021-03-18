c02677021edeb4cc4f45ac2443dc8efee2f964cf
jpclipffel <jpclipffel@users.noreply.github.com>
2021-03-18 13:48:27 +0100

    Code cleanup

--- debug.sh
+++ debug.sh
@@ -7,15 +7,19 @@ function __debug_echo() {
 
 
 # macOS (Darwin) tests
-function __debug_darwin() {
+function __debug_darwin()
+{
     # [Check] Is xcrun installed ?
+    # This is required to run 'brew'.
     xcrun --version &> /dev/null || __debug_echo "Command 'xcrun' failed: try (re)installing Xcode toolkit: xcode-select --install"
+
     # [Check] Is brew installed ?
     type brew &> /dev/null || __debug_echo "brew is not installed: install it from https://brew.sh/"
+
     # [Debug] Remove signature control for VSCode render to avoid shell lagging
     # See https://github.com/microsoft/vscode/issues/105446
     if [ -e "/Applications/Visual Studio Code.app/Contents/Frameworks/Code Helper (Renderer).app" ]; then
-        codesign --remove-signature "/Applications/Visual Studio Code.app/Contents/Frameworks/Code Helper (Renderer).app"
+        codesign --remove-signature "/Applications/Visual Studio Code.app/Contents/Frameworks/Code Helper (Renderer).app" &> /dev/null
     fi
 }
 
