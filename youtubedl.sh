2522f911a8666dcb5ffb387b0435bae3098b58e0
jpclipffel <jpclipffel@users.noreply.github.com>
2021-12-23 11:58:41 +0100

    [Fix] Use current directory by default

--- youtubedl.sh
+++ youtubedl.sh
@@ -6,30 +6,32 @@ export ydl_archive_filename="youtube-dl-archive.txt"
 # Updates a youtube-dl repository
 # ${1}: Repository directory
 function ydl_update() {
+    local dir="${1:-.}"
     # Assertions
-    [[ -z "${1}" ]] && echo "Usage: $(basename ${0}) <directory>" && return $(false);
-    [[ ! -d "${1}" ]] && echo "Error: '${1}' does not exists or is not a directory" && return $(false);
-    [[ ! -f "${1}/${ydl_cfg_filename}" ]] && echo "Error: configuration file '${1}/${ydl_cfg_filename}' does not exists or is not a file" && return $(false);
-    [[ ! -f "${1}/${ydl_urls_filename}" ]] && echo "Error: URLs file '${1}/${ydl_urls_filename}' does not exists or is not a file" && return $(false);
+    [[ -z "${dir}" ]] && echo "Usage: $(basename ${0}) <directory>" && return $(false);
+    [[ ! -d "${dir}" ]] && echo "Error: '${1}' does not exists or is not a directory" && return $(false);
+    [[ ! -f "${dir}/${ydl_cfg_filename}" ]] && echo "Error: configuration file '${dir}/${ydl_cfg_filename}' does not exists or is not a file" && return $(false);
+    [[ ! -f "${dir}/${ydl_urls_filename}" ]] && echo "Error: URLs file '${dir}/${ydl_urls_filename}' does not exists or is not a file" && return $(false);
     # Update
-    cd "${1}"
+    cd "${dir}"
     youtube-dl --config-location "${ydl_cfg_filename}"
 }
 
 # (re)Configures a youtube-dl repository
 # ${1}: Repository directory
 function ydl_configure() {
+    local dir="${1:-.}"
     # Assertions
-    [[ -z "${1}" ]] && echo "Usage: $(basename ${0}) <directory>" && return $(false);
-    [[ ! -d "${1}" ]] && echo "Error: '${1}' does not exists or is not a directory" && return $(false);
+    [[ -z "${dir}" ]] && echo "Usage: $(basename ${0}) <directory>" && return $(false);
+    [[ ! -d "${dir}" ]] && echo "Error: '${dir}' does not exists or is not a directory" && return $(false);
     # Save current configuration
-    if [ -f "${1}/${ydl_cfg_filename}" ]; then
-        echo "Saving current youtube-dl configuration file as ${1}/${ydl_cfg_filename}.bak"
-        cp "${1}/${ydl_cfg_filename}" "${1}/${ydl_cfg_filename}.bak"
+    if [ -f "${dir}/${ydl_cfg_filename}" ]; then
+        echo "Saving current youtube-dl configuration file as ${dir}/${ydl_cfg_filename}.bak"
+        cp "${dir}/${ydl_cfg_filename}" "${dir}/${ydl_cfg_filename}.bak"
     fi
     # Write new configuration
     echo "Writing youtube-dl configuration file at ${ydl_cfg_filename}"
-    cat > "${1}/${ydl_cfg_filename}" << EOF
+    cat > "${dir}/${ydl_cfg_filename}" << EOF
 # General Options
 --ignore-errors
 
