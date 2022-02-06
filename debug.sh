6228035191e9f8ed592fe79a900729b5129e95ec
jpclipffel <jpclipffel@users.noreply.github.com>
2022-02-06 14:29:34 +0100

    [Debug] Added support for ${__BREW} var

--- debug.sh
+++ debug.sh
@@ -14,7 +14,13 @@ function __debug_darwin()
     xcrun --version &> /dev/null || __debug_echo "Command 'xcrun' failed: try (re)installing Xcode toolkit: xcode-select --install"
 
     # [Check] Is brew installed ?
-    type brew &> /dev/null || __debug_echo "brew is not installed: install it from https://brew.sh/"
+    # The script 'brew.sh' overwrite the command 'brew' and use Brew's path through the variable ${__BREW}.
+    # First condition check if the variable ${__BREW} is set; Otherwise fallback to standard command existence check.
+    if [[ -n "${__BREW+1}" ]]; then
+        [[ -f "${__BREW}" ]] || echo "Brew not found in '${__BREW}': set \${__BREW} and/or install Brew from https://brew.sh"
+    else
+        type brew &> /dev/null || __debug_echo "brew is not installed: install it from https://brew.sh/"
+    fi
 
     # [Debug] Remove signature control for VSCode render to avoid shell lagging
     # See https://github.com/microsoft/vscode/issues/105446
