60aa5c7fab44ffd1115bd8de0f700f9190662fef
jpclipffel <jpclipffel@users.noreply.github.com>
2022-02-06 14:53:08 +0100

    [Feat] Replaced ssh by xxh and added _ssh for raw ssh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,18 @@
+__SSH="${__SSH:=/usr/bin/ssh}"
+
+
 # List SSH hosts
 function ssh_hosts() {
-  cat ${HOME}/.ssh/config | grep '^Host' | cut -d ' ' -f 2
+    cat ${HOME}/.ssh/config | grep '^Host' | cut -d ' ' -f 2
+}
+
+
+# Replace SSH with XXH is XXH is installed
+function ssh() {
+    type xxh &>/dev/null && xxh "${@}" || (echo "xxh not installed, fallback to ssh" && "${__SSH}" "${@}")
+}
+
+
+function _ssh() {
+    "${__SSH}" "${@}"
 }
