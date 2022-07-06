2167623c54ff1ff187fa608a4f7dacd1149ea57e
jpclipffel <jpclipffel@users.noreply.github.com>
2022-07-06 07:59:21 +0200

    [Fix] Prepen home

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,6 @@
 # Lists SSH hosts
 function ssh-hosts() {
-    grep '^Host' .ssh/config .ssh/config.d/* \
+    grep '^Host' ~/.ssh/config ~/.ssh/config.d/* \
     | sed 's/\(.*\):Host \(.*\)/\2 : \1/' \
     | column -t -s ':'
 }
