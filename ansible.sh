943fc07b3d0893e7253f613e7b14850f2843b84e
Jean-Philippe Clipffel <jeanphilippe.clipffel@explorajourneys.com>
2023-09-30 11:09:42 +0200

    [Feat] Added ARA support

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,14 @@
+# Ansible :: ARA :: Container
+# - Non-standard variables
+export ARA_CONTAINER_NAME="ansible-ara"
+export ARA_CONTAINER_LOCAL_PORT=8000
+
+# Ansible :: ARA
+# - Standard variables
+export ARA_API_CLIENT="http"
+export ARA_API_SERVER="http://127.0.0.1:${ARA_CONTAINER_LOCAL_PORT}"
+
+
 # Install Ansible
 # NB: Do not use 'read -p "prompt"' or 'read "foo?prompt"' to keep
 # compatibility between Bash and ZSH
@@ -10,7 +21,7 @@ function ansible-install() {
     echo "Please select Ansible flavour ('ansible' or 'ansible-core'): "
     read "flavour"
     case "${flavour}" in
-        ansible) pip install ansible ansible-lint ansible-vault;;
+        ansible) pip install ansible ansible-lint ansible-vault ara;;
         ansible-core) pip install ansible-core ansible-lint ansible-vault;;
     esac
 }
@@ -24,6 +35,40 @@ function ansible-update() {
     pip list --format "freeze" | grep "ansible" && packages+=("ansible")
     # Update
     pip install --upgrade "${packages[@]}"
+}
+
+
+# Run ARA
+function ansible-ara() {
+    case "${1}" in
+        start)
+            docker run --name ${ARA_CONTAINER_NAME} \
+            --detach \
+            --tty \
+            --volume ${HOME}/.ara/server:/opt/ara \
+            -p ${ARA_CONTAINER_LOCAL_PORT}:8000 \
+            docker.io/recordsansible/ara-api:latest
+            ;;
+        status)
+            docker ps | grep ${ARA_CONTAINER_NAME}
+            ;;
+        open)
+            open "${ARA_API_SERVER}"
+            ;;
+        *)
+            echo "Usage: ${0} <start|status|open|stop>"
+            return 1
+            ;;
+    esac
+}
+
+
+# Run Ansible playbook with ARA as callback plugin
+function ansible-playbook-ara() {
+    ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)" \
+    ANSIBLE_ACTION_PLUGINS="$(python3 -m ara.setup.action_plugins)" \
+    ANSIBLE_LOOKUP_PLUGINS="$(python3 -m ara.setup.lookup_plugins)" \
+    ansible-playbook "${@}"
 }
 
 
